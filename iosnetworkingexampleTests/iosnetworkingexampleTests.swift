da11f7533a50deadc2d15397e27f319bffc75f37
msabukwaik <msabukwaik@gmail.com>
2017-09-19 18:47:19 +0300

    change root folder

--- iosnetworkingexampleTests/iosnetworkingexampleTests.swift
+++ iosnetworkingexampleTests/iosnetworkingexampleTests.swift
@@ -11,21 +11,14 @@ import XCTest
 
 class iosnetworkingexampleTests: XCTestCase {
     
-    var viewController:ViewController!
-    
-    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        let storyBoard = UIStoryboard(name: "Main", bundle: Bundle.main)
-        viewController = storyBoard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
-        _ = viewController.view
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-        viewController = nil
     }
     
     func testExample() {
@@ -37,33 +30,6 @@ class iosnetworkingexampleTests: XCTestCase {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
-        }
-    }
-    
-    func testSayHello() {
-        print("Hello world")
-    }
-    
-    func testGetImageFromFlicker() {
-        let expection = expectation(description: "Waiting for testing")
-        var img:UIImage?
-        
-        print(viewController.sayHi())
-        viewController.getImageFromFlicker { (image, error) in
-            if error == nil && image != nil{
-                expection.fulfill()
-                img = image
-            }
-        }
-        
-        self.waitForExpectations(timeout: 10) { (error) in
-            if error == nil{
-                print("Image exists")
-                XCTAssert(img != nil)
-            }else{
-                print(error.debugDescription)
-            }
-            
         }
     }
 }
