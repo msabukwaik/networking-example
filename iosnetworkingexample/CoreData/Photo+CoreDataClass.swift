39e8c47561b0fa6754784db97c029f698794e10b
msabukwaik <msabukwaik@gmail.com>
2017-10-09 18:57:40 +0300

    add photo core data

---
+++ iosnetworkingexample/CoreData/Photo+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Photo+CoreDataClass.swift
+//  iosnetworkingexample
+//
+//  Created by Mohammed S A Kwaik on 10/9/17.
+//  Copyright © 2017 Mohammed S A Kwaik. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Photo)
+public class Photo: NSManagedObject {
+
+}
